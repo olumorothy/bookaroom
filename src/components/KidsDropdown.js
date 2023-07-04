290f5e22e94989d2d88202b6140a67750fe23d07
Olumorothy <iseoluwa89@gmail.com>
2023-07-04 15:24:06 +0100

    adds styling for kids and adults dropdown

--- src/components/KidsDropdown.js
+++ src/components/KidsDropdown.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { RoomContext } from "../context/RoomContext";
+import { Menu } from "@headlessui/react";
+import { BsChevronDown } from "react-icons/bs";
 
-const KidsDropdown = () => {
-  return <div>KidsDropdown</div>;
+const lis = [
+  {
+    name: "0 Kids ",
+  },
+  {
+    name: "1 Kid",
+  },
+  {
+    name: "2 Kids",
+  },
+  {
+    name: "3 Kids",
+  },
+  {
+    name: "4 Kids",
+  },
+];
+const KidsDropDown = () => {
+  return (
+    <Menu as="div" className="w-full h-full bg-white relative">
+      <Menu.Button className="w-full h-full flex items-center justify-between px-8">
+        kids
+        <BsChevronDown className="text-base text-accent-hover" />
+      </Menu.Button>
+      <Menu.Items
+        as="ul"
+        className="bg-white absolute w-full flex flex-col z-40"
+      >
+        {lis.map((li, index) => {
+          return (
+            <Menu.Item
+              as="li"
+              className="border-b last-of-type:border-b-0 h-12 hover:bg-accent hover:text-white w-full flex justify-center items-center cursor-pointer"
+              key={index}
+            >
+              {li.name}
+            </Menu.Item>
+          );
+        })}
+      </Menu.Items>
+    </Menu>
+  );
 };
 
-export default KidsDropdown;
+export default KidsDropDown;
