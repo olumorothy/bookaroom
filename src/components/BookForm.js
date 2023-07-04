164bb63f3d85ce9cb8f55ee6169e67eeeed83448
Olumorothy <iseoluwa89@gmail.com>
2023-07-04 14:13:38 +0100

    adds design for bookform

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
 
+import AdultsDropdown from "../components/AdultsDropdown";
+import KidsDropdown from "../components/KidsDropdown";
+import CheckIn from "../components/CheckIn";
+import CheckOut from "../components/CheckOut";
 const BookForm = () => {
-  return <div>BookForm</div>;
+  return (
+    <form className="h-[300px] bg-pink-200 w-full lg:h-[70px]">
+      <div className="flex flex-col w-full h-full lg:flex-row">
+        <div className="flex-1 border-r">
+          <CheckIn />
+        </div>
+        <div className="flex-1 border-r">
+          <CheckOut />
+        </div>
+        <div className="flex-1 border-r">
+          <AdultsDropdown />
+        </div>
+        <div className="flex-1 border-r">
+          <KidsDropdown />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default BookForm;
