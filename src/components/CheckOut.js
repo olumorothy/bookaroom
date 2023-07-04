7fde077de8a3bab2d93448c00b16c2f5c08156d0
Olumorothy <iseoluwa89@gmail.com>
2023-07-04 14:52:11 +0100

    adds styling for checkout component

--- src/components/CheckOut.js
+++ src/components/CheckOut.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import ReactDatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
+import "../datepicker.css";
+import { BsCalendar } from "react-icons/bs";
 const CheckOut = () => {
-  return <div>CheckOut</div>;
+  const [endDate, setEndDate] = useState(false);
+  return (
+    <div className="relative flex items-center justify-end h-full">
+      <div className="absolute z-10 pr-8">
+        <div>
+          <BsCalendar className="text-accent text-base" />
+        </div>
+      </div>
+      <ReactDatePicker
+        className="w-full h-full"
+        selected={endDate}
+        placeholderText="Check Out"
+        onChange={(date) => setEndDate(date)}
+      />
+    </div>
+  );
 };
 
 export default CheckOut;
